7f67d7bd6316043b349d0dc3f49fa716820ba91c
Jeffrey Lomibao <jeffrey.lomibao@georgfischer.com>
2024-09-19 16:19:27 -0700

    Module 1 homework

---
+++ C++ for C Programmers, Part A/Module1/sum_of_vector.cpp
@@ -0,0 +1,33 @@
+// Convert this program to C++
+// change to C++ io
+// change to one line comments
+// change defines of constants to const
+// change array to vector<>
+// inline any short function
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <typename summable>
+summable sum(vector<summable> d)
+{
+    summable sum{0};
+    for (int i{0}; i < d.size(); ++i)
+        sum += i;
+    return sum;
+}
+
+int main()
+{
+    // generate test data
+    vector<int> data;
+    const int DATA_SIZE = 40;
+    for (int i{0}; i < DATA_SIZE; ++i)
+        data.push_back(i);
+
+    cout << "sum is " << sum(data) << endl;
+
+    return 0;
+}
