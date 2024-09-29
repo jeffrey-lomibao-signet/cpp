61ee0e25113bebe44972ab2a5b7ac12235b7c506
Jeffrey Lomibao <jeffrey.lomibao@georgfischer.com>
2024-09-29 09:48:53 -0700

    Start creating random graph

--- C++ for C Programmers, Part A/Module2/homework/dijkstra.cpp
+++ C++ for C Programmers, Part A/Module2/homework/dijkstra.cpp
@@ -545,6 +545,29 @@ void testWikipediaGraph() {
 }
 
 //=============================================================================
+
+// Basic problem:  
+// Write a set of constructors for declaring and initializing a graph. 
+// An edge will have a positive cost that is its distance. 
+// Have a procedure that produces a randomly generated set of edges with positive distances.  
+// Assume the graphs are undirected. 
+// The random graph procedure should have edge density as a parameter,
+// and the distance range as a parameter. 
+// So a graph whose density is 0.1 would have 10% of its edges picked at random.
+// Its edge distance would be selected at random from the distance range. 
+// The procedure should run through all possible undirected edges, say (i,j). 
+// Place the edge in the graph if a random probability calculation is less than the density. 
+// Compute for a set of randomly generated graphs an average shortest path. 
+
+Graph createRandomGraph(size_t numNodes) {
+  Graph g(numNodes);
+
+  cout << "Vertices = " << g.getNumVertices() << "; Edges = " << g.getNumEdges() << endl;
+  cout << "Random Graph:" << endl << g;
+  return g;
+}
+
+//=============================================================================
 int main() {
   testExampleGraph();
   testWikipediaGraph();
